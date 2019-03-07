5c464a93f76948bbd7a097571a1286ae4ddf6c42
mayaramirez01 <47905318+mayaramirez01@users.noreply.github.com>
2019-03-07 14:25:50 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 var a= document.getElementById("el");
 var b= document.getElementById("el2");
 var c= document.getElementById("el3");
+var d= document.getElementById("el");
 function nextImage(el){
 if (a.src.match("images/no_color_eyes.jpg")){ 
-		a.src= "images/eye_color_four.png"; 
-	}else if (a.src.match("images/eye_color_four.png")){
+		a.src= "images/final_eye_look.png"; 
+	}else if (a.src.match("images/final_eye_look.png")){
 		a.src= "images/no_color_eyes.jpg";
 	}
 	
@@ -21,9 +22,21 @@ if (b.src.match("images/bare_cheek.jpg")){
 
 function nextImage3(el){
 if (c.src.match("images/lips.PNG")){ 
-		c.src= "images/lip_color.png"; 
-	}else if (c.src.match("images/lip_color.png")){
+		c.src= "images/lip_color.PNG"; 
+	}else if (c.src.match("images/lip_color.PNG")){
 		c.src= "images/lips.PNG";
 	}
 	
 }
+function nextImage4(el){
+if (d.src.match("images/no_color_eyes.jpg")){ 
+		d.src= "images/eyes_color_one.png"; 
+	}else if (d.src.match("images/eyes_color_one.png")){
+		d.src= "images/no_color_eyes.jpg";
+	}
+	
+}
+
+function refreshPage(){
+    window.location.reload();
+} 
