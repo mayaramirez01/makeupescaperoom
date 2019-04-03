c73ae5b4e6bbe9343571e22160175c1694cc3f53
mayaramirez01 <47905318+mayaramirez01@users.noreply.github.com>
2019-04-03 08:41:30 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ var a= document.getElementById("el");
 var b= document.getElementById("el2");
 var c= document.getElementById("el3");
 var d= document.getElementById("el4");
-
+//above i made variables for my element IDś to reduce the amount of code I would need to type out.
+// all of my nextImage functions are linked to certain divs in my html with the onClick attribute and ID
 function nextImage(el){
 if (a.src.match("images/no_color_eyes.jpg")){ 
 		a.src= "images/final_eye_look.PNG"; 
@@ -35,7 +36,8 @@ if (c.src.match("images/lips.PNG")){
 function refreshPage(){
     window.location.reload();
 } 
-
+//This function is what checks if the user completed the game in the correct way 
+//and if itś correct it removes the opaque class and makes the key image clickable.
 function checkFaces(){ 
 	if (a.src.match("images/final_eye_look.png") && b.src.match("images/cheeks.jpg") && c.src.match("images/lip_color.PNG")){
 		d.classList.remove("opaque");
